3a5a407ede694a2ebee7c6e74d36a23305ba3f04
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-15 15:13:30 -0300

    Terceira Aula
    01:04:00 (pausa)

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -69,7 +69,7 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
             {allEpisodes.map((episode) => {
               return (
                 <tr key={episode.id}>
-                  <td>
+                  <td style={{width: 72}}>
                     <Image
                       width={120}
                       height={120}
@@ -82,7 +82,7 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
                     <a href="">{episode.title}</a>
                   </td>
                   <td>{episode.members}</td>
-                  <td>{episode.publishedAt}</td>
+                  <td style={{width: 100}}>{episode.publishedAt}</td>
                   <td>{episode.durationAsString}</td>
                   <td>
                     <button type="button">
