31fc15bd2ff4cff84b6517f0b71acad50538365d
Hugo <hugovarellaa@gmail.com>
2022-01-22 10:47:09 -0300

    Criando e testando as rotas no insomnia

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { application } from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Rota get ok" });
+});
+app.post("/", (req, res) => {
+  return res.json({ message: "Rota post ok" });
+});
+
+app.listen(3333, () => console.log("Server is run in port 3333"));
