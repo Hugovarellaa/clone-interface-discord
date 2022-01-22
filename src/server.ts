7494a6f813790ac75d8eeb845e246786350e0358
Hugo <hugovarellaa@gmail.com>
2022-01-22 18:25:33 -0300

    criando os repositories e criando as configurações de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import "./database";
 import express from "express";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Rota get ok" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "Rota post ok" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is run in port 3333"));
