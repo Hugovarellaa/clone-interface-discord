cf9ab66aa9bd867f959691dc0b7d251a4111052e
Hugo <hugovarellaa@gmail.com>
2022-01-24 11:00:12 -0300

    Criando migration settings

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
 import express from "express";
+import "./database";
 
 const app = express();
-
-app.get("/", (req, res) => {
-  return res.json("rota get funcionando");
-});
-
-app.post("/", (req, res) => {
-  return res.json("rota post funcionando");
-});
 
 app.listen(3333, () => console.log("Serve is running in port 3333"));
