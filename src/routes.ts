7b587e4ee0dcfedd3f7f8783f89100d9c842dfe4
Hugo <hugovarellaa@gmail.com>
2022-01-24 11:58:10 -0300

    Criando o controller do settings e refatorando

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,10 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controller/SettingsController";
 
 const routes = Router();
 
-routes.post("/settings", async (req, res) => {
-  const { chat, username } = req.body;
-  const settingsRepository = getCustomRepository(SettingsRepository);
+const settingsRepository = new SettingsController();
 
-  const settings = settingsRepository.create({
-    chat,
-    username,
-  });
-
-  await settingsRepository.save(settings);
-
-  return res.json(settings);
-});
+routes.post("/settings", settingsRepository.create);
 
 export { routes };
