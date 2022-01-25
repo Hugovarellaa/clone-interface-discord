932f948f3a0f0c8682c460498beddc5138b7bbf5
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-01-25 12:18:42 -0300

    Delete src directory

--- src/repositories/MessagesRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Message } from "../entities/Message";
-
-@EntityRepository(Message)
-class MessagesRepository extends Repository<Message> {}
-
-export { MessagesRepository };
--- src/repositories/SettingsRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Setting } from "../entities/Setting";
-
-@EntityRepository(Setting)
-class SettingsRepository extends Repository<Setting> {}
-
-export { SettingsRepository };
