bd8250e4ad3abf2f222356dccdc31a4035778d08
Hugo <hugovarellaa@gmail.com>
2022-01-25 10:33:17 -0300

    importando arquivos do front-end ... instalando e configurando o socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express from "express";
 import "./database";
 import { routes } from "./routes";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
 
 const app = express();
+app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "../public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (req, res) => {
+  return res.render("html/client.html");
+});
+
+const http = createServer(app); //Criando protocolo http
+const io = new Server(http); //Criando protocolo Websocket
+
+io.on("Connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
+
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log("Serve is running in port 3333"));
+http.listen(3333, () => console.log("Serve is running in port 3333"));
